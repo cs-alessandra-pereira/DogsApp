57f9e5b920c3d7911f951bf874efd1b2e2a1d2c9
Alessandra <alessandra.pereira@concrete.com.br>
2018-03-20 14:16:41 -0300

    Conteúdo incompleto da primeira aula

---
+++ DogsAppTests/Datasources/FavoritesDatasourceSpec.swift
@@ -0,0 +1,58 @@
+//
+//  FavoritesDatasourceSpec.swift
+//  DogsAppTests
+//
+//  Created by alessandra.l.pereira on 19/03/18.
+//  Copyright © 2018 Thiago Lioy. All rights reserved.
+//
+
+@testable import DogsApp
+import XCTest
+
+class FavoritesDatasourceSpec: XCTestCase {
+    
+    var tableView: UITableView!
+    var sammy: Breed!
+    var duke: Dog!
+    var garrel: Dog!
+    var dogs: [Dog]!
+    var sut: FavoritesDatasource!
+    
+    override func setUp() {
+        //Arrange
+        tableView = UITableView(frame: .zero)
+        sammy = Breed(name: "Samoyed")
+        duke = Dog(breed: sammy, image: "")
+        garrel = Dog(breed: sammy, image: "")
+        dogs = [duke, garrel]
+        sut = FavoritesDatasource.init(tableView: tableView, dogs: dogs)
+    }
+    
+    func testSanitize(){
+        XCTAssertTrue(true)
+    }
+    
+    
+    
+    func testNumberOfRowsInSectionShouldReturnCountOfDogsBreed(){
+
+        //Act
+        let rows = sut.tableView(tableView, numberOfRowsInSection: 0)
+        
+        //Assert
+        XCTAssertTrue(rows == dogs.count)
+    }
+    
+    func testCellForRowAtIndexPathShouldReturnAValidCell(){
+        
+        //Arrange
+        let indexPath = IndexPath(row: 0, section: 0)
+        
+        //Act
+        let cell = sut.tableView(tableView, cellForRowAt: indexPath) as? FavoriteDogTableViewCell
+        
+        //Assert
+        XCTAssertNotNil(cell)
+        
+    }
+}
---
+++ DogsAppTests/Datasources/FavoritesDatasourceStub.swift
@@ -0,0 +1,9 @@
+//
+//  FavoritesDatasourceStub.swift
+//  DogsAppTests
+//
+//  Created by alessandra.l.pereira on 19/03/18.
+//  Copyright © 2018 Thiago Lioy. All rights reserved.
+//
+
+import Foundation
